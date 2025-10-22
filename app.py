017ee7f5a271b61bff3bcaee52089d74e95b3d06
Pocverdn <santisanches538@gmail.com>
2025-10-22 00:50:38 -0500

    Correción app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 from extensions import db, login_manager
 from models.user import User
+import config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookstore.db'
+app.config.from_object(config)
 
 db.init_app(app)
 login_manager.init_app(app)
